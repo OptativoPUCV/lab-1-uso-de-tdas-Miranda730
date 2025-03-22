efab5791a593bec0ec642326ddfcfc1aafbf0e6f
Your Name <you@example.com>
2025-03-21 22:02:49 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -43,8 +43,14 @@ Al finalizar retorna la lista creada.
 
 List* crea_lista() {
    List* L = create_list();
+   for(int i = 1 ; i <= 10 ; i++) {
+      int*dato = (int*)malloc(sizeof(int));
+      *dato = i;
+      pushBack(L, dato);
+   }
    return L;
 }
+
 
 /*
 Ejercicio 2.
@@ -52,8 +58,15 @@ Crea una función que reciba una lista de enteros (int*) y
 retorne la suma de sus elementos.
 */
 int sumaLista(List *L) {
-   return 0;
+   int suma = 0;
+   int* dato = (int*)first(L);
+   while(dato != NULL){
+      suma += *dato;
+      dato = (int*)next(L);
+   }
+   return suma;
 }
+
 
 /*
 Ejercicio 3.
@@ -65,8 +78,17 @@ posiciona en el elemento anterior.
 */
 
 void eliminaElementos(List*L, int elem){
-
+   int* dato = (int*)first(L);
+   while(dato != NULL){
+      if(*dato == elem){
+         popCurrent(L);
+         dato = (int*)first(L);
+      } else{
+         dato = (int*)next(L);
+      }
+   }
 }
+
 
 /*
 Ejercicio 4.
@@ -76,7 +98,22 @@ Puedes usar una pila auxiliar.
 */
 
 void copia_pila(Stack* P1, Stack* P2) {
+   Stack* pila_aux = create_stack();
+   
+   while(top(P1) != NULL){
+      void* elemento = top(P1);
+      push(pila_aux, elemento);
+      pop(P1);
+   }
+
+   while(top(pila_aux) != NULL){
+      void* elemento = top(pila_aux);
+      push(P1, elemento);
+      push(P2, elemento);
+      pop(pila_aux); 
+   }
 }
+
 
 /*
 Ejercicio 5.
@@ -86,6 +123,39 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 */
 
 int parentesisBalanceados(char *cadena) {
-   return 0;
+   Stack *pila = create_stack();
+
+   for(int i = 0 ; cadena[i] != '\0' ; i++){
+      char dato = cadena[i];
+      if(dato == '(' || dato == '{' || dato == '['){
+         char *nuevoDato = (char)malloc(sizeof(char));
+         *nuevoDato = dato;
+         push(pila, nuevoDato);
+
+      }else if(dato == ')' || dato == '}' || dato == ']'){
+         if(top(pila) == NULL){
+            return 0;
+         }
+         
+         char *cima = (char)top(pila);
+         if((dato == ')' && *cima != '(') || 
+            (dato == '}' && *cima != '{') || 
+            (dato == ']' && *cima != '[')){
+            return 0;
+         }
+         free(pop(pila));
+      }
+   }
+   int resultado;
+   if(top(pila) == NULL){
+      resultado = 1;
+   }else{
+      resultado = 0;
+   }
+
+   while(top(pila) != NULL){
+      free(pop(pila));
+   }
+   return resultado;
 }
 
